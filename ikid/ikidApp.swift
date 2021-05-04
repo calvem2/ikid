6446bf98704b5c3415631affd0c8f02f28e14a4a
Megan <calvem2@cs.washington.edu>
2021-05-04 09:54:19 -0700

    Initial Commit

---
+++ ikid/ikidApp.swift
@@ -0,0 +1,17 @@
+//
+//  ikidApp.swift
+//  ikid
+//
+//  Created by Megan on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct ikidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
